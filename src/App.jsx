d75b70a65784a8d819946960bf2f82bc27873d8b
2310102003dwinia-gif <2310102003.dwinia@student.tazkia.ac.id>
2026-04-05 23:26:28 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Sharia Accounting Dashboard</h1>
+      <p>Deploy berhasil 🚀</p>
+    </div>
+  );
+}
+
+export default App;
